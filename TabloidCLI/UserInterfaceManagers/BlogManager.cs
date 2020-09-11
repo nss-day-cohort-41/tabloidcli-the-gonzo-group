e464691c57cc0b2d5d455668806b353c2bb0d0a8
Nicholas Glover <nickglovdev@gmail.com>
2020-09-11 13:20:16 -0500

    You can now see the blog menu as well as being able to see the title of the blog and URL in pressing number 1

--- TabloidCLI/UserInterfaceManagers/BlogManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -21,9 +21,30 @@ namespace TabloidCLI.UserInterfaceManagers
 
         public IUserInterfaceManager Execute()
         {
-            Console.WriteLine("");
+            Console.WriteLine("Blog Menu");
             Console.WriteLine(" 1) List Title and URL");
-            throw new NotImplementedException();
+            Console.WriteLine(" 0) To Exit Blog");
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+        private void List()
+        {
+            List<Blog> blogs = _blogRepository.GetAll();
+            foreach (Blog blog in blogs)
+            {
+                Console.WriteLine(blog);
+            }
         }
     }
 }
