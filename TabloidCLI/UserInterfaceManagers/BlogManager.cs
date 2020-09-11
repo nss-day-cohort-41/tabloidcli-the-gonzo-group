bb8f18e9bcaf99bb179345daa37ddd7e3620bf96
Nicholas Glover <nickglovdev@gmail.com>
2020-09-11 12:51:34 -0500

    Finished up the BlogRepository now working on BlogManager.

---
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class BlogManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+        public BlogManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" 1) List Title and URL");
+            throw new NotImplementedException();
+        }
+    }
+}
